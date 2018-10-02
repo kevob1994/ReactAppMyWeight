0136c20d05dada2abfac3807ebdf58ab120d4fe8
Kevin Blanco <kblanco@novopayment.com>
2018-10-02 10:01:56 -0400

    Importa componentes de chart y tabla de datos, remueve funciones

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
+import moment from 'moment';
+import Chart from './components/Chart'
+import DataInfo from './components/DataInfo';
 
+moment.locale('es')
 class App extends Component {
+  state = {
+    registros: [0,1,2,3,4,5,6].map( dia =>
+      [+moment().add(dia, 'days'), Math.random()*200] 
+    )
+  }
+
+  getRegistros = () => {
+    var fechas = [0,1,2,3,4,5,6].map( dia =>
+      [+moment().add(dia, 'days'), Math.random()*200] 
+    )
+    console.log(fechas);
+    
+    return fechas;
+  }
+    
+  
+  componentDidMount(){
+
+  }
+
   render() {
+    let { registros } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <NavBar />
+        <main>
+          <div className="valign-wrapper">
+            <h2>Registro Diario de Peso</h2>
+          </div>
+          <div className="row">
+            <div className="col s6">
+              <Chart registros={registros} />
+            </div>
+            <div className="col s6">
+              <DataInfo registros = {registros} />
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
